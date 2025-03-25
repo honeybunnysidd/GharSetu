7cfe592e174be1c79de7666e5ef13aa3835843b3
Siddhart Raghuvanshi <siddhartharaghuvanshi01@gmail.com>
2025-03-25 08:48:32 +0530

    add file

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const { Listing } = require("./models/listing");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.engine("ejs", ejsMate);
 
 //Database Connection
 let MONGO_URL = "mongodb://127.0.0.1:27017/GharSetu";
