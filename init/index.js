4eb3e650de23bb3ca3d114e051d2743b5425b78d
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-04-07 16:47:50 +0530

    Update file

--- init/index.js
+++ init/index.js
@@ -1,10 +1,15 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
 const mongoose = require("mongoose");
 let { data } = require("./data");
 const Listing = require("../models/listing");
 const Review = require("../models/review");
 
 //Database Connection
-let MONGO_URL = "mongodb://127.0.0.1:27017/GharSetu";
+// let MONGO_URL = "mongodb://127.0.0.1:27017/GharSetu";
+let MONGO_URL = process.env.ATLASDB_URL;
+
 main()
   .then(() => {
     console.log("DB Connected Sucessfully");
