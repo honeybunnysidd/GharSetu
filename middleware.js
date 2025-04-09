9da461f17a026ca20d0f98b895c08fd9c14da2f4
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-04-09 10:30:48 +0530

    fix bugs

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ const Review = require("./models/review.js");
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
-    req.flash("error", "User must be not logged in to create new listing!");
+    req.flash("error", "User must logged into create new listing!");
     return res.redirect("/login");
   }
   next();
